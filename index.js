36383d89474b4d26a36840712015ef11733694f0
Karlova Tetiana <tanyalyt80@gmail.com>
2024-03-27 22:43:38 +0200

    added animation, upgrade

---
+++ index.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 200,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+});
+
+sr.reveal('.animate-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400,
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+})
